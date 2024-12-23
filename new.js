2740633a9aad1a8794261f39874ea464886f783b
chiragmehra1220002 <chiragmehra265@gmail.com>
2024-12-23 17:16:26 +0530

    new json things add

--- new.js
+++ new.js
@@ -3,8 +3,8 @@ for(let i in arr2)
 {
     console.log(arr2[i]);
 }
-var json = require('./config.json');
-console.log(json.username);
-console.log(json.password);
+import { username, password } from './config.json';
+console.log(username);
+console.log(password);
 
 
